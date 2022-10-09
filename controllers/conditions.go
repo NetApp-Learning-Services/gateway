92a2ef9be85e52bfecee6f5334323d9a035f2aa7
cburchett <curtis@burchett.me>
2022-10-09 18:13:50 -0500

    Changed the condition text

--- controllers/conditions.go
+++ controllers/conditions.go
@@ -11,9 +11,9 @@ const CONDITION_STATUS_FALSE = "False"
 const CONDITION_STATUS_UNKNOWN = "Unknown"
 
 // Note: Status of RESOURCE_FOUND can only be True; otherwise there is no condition
-const CONDITION_TYPE_RESOURCE_FOUND = "ResourceFound"
+const CONDITION_TYPE_RESOURCE_FOUND = "ResourceDiscovered"
 const CONDITION_REASON_RESOURCE_FOUND = "ResourceFound"
-const CONDITION_MESSAGE_RESOURCE_FOUND = "Resource found in k18n"
+const CONDITION_MESSAGE_RESOURCE_FOUND = "Resource found by gateway operator"
 
 func (reconciler *StorageVirtualMachineReconciler) setConditionResourceFound(ctx context.Context,
 	svmCR *gatewayv1alpha1.StorageVirtualMachine) error {
