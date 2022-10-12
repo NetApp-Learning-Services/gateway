6e599990c5c47a79a3e688990312cf90a5e73926
cburchett <curtis@burchett.me>
2022-10-12 15:40:10 -0500

    Update test script to delete the CR to complete the finalizer

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # A simple bash script to reset and test the application
 
+kubectl -n gateway-system delete -f notes/testCR.yaml
 kubectl delete namespace gateway-system 
 git pull origin
 make docker-build docker-push IMG=docker-registry:30001/astra/gateway:v0.1
