992d3c6188a0ee528fe0c90bcda5961eaca47ee6
cburchett <curtis@burchett.me>
2022-10-25 21:43:17 -0500

    Updated test script to ensure proper deletion

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # A simple bash script to reset and test the application
 
-kubectl -n gateway-system delete -f notes/testCR.yaml
+kubectl -n gateway-system delete svm storagevirtualmachine-sample
+kubectl -n gateway-system delete secret ontap-cluster-admin
+kubectl -n gateway-system delete secret ontap-svm-admin
 kubectl delete namespace gateway-system 
 git pull origin
 make docker-build docker-push IMG=docker-registry:30001/astra/gateway:v0.1
