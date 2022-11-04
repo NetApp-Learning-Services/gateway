934596270dad228ff0b86a4f837caef585fd8f58
cburchett <curtis@burchett.me>
2022-11-04 14:42:09 -0500

    changing docker image name

--- test.sh
+++ test.sh
@@ -6,6 +6,6 @@ kubectl -n gateway-system delete secret ontap-cluster-admin
 kubectl -n gateway-system delete secret ontap-svm-admin
 kubectl delete namespace gateway-system 
 git pull origin
-make docker-build docker-push IMG=docker-registry:30001/astra/gateway:v0.1
+make docker-build docker-push IMG=docker-registry:30001/curtisab/gateway:v1alpha1
 make deploy
 kubectl -n gateway-system create -f notes/testCR.yaml
