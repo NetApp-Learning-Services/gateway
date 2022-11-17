053427a209e0d804e017c116782a22e319259176
cburchett <curtis@burchett.me>
2022-11-17 14:52:12 -0600

    Working on getting v1alpha2 to work

--- test.sh
+++ test.sh
@@ -6,6 +6,6 @@ kubectl -n gateway-system delete secret ontap-cluster-admin
 kubectl -n gateway-system delete secret ontap-svm-admin
 kubectl delete namespace gateway-system 
 git pull origin v1alpha2
-make docker-build docker-push IMG=docker-registry:30001/curtisab/gateway:v1alpha1
+make docker-build docker-push IMG=docker-registry:30001/curtisab/gateway:v1alpha2
 make deploy
 kubectl -n gateway-system create -f notes/testCR.yaml
