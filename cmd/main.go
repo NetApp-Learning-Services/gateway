403fb4c2579e7a84beb0f8712a5b4cfe2277b81e
cburchett <curtis@burchett.me>
2024-01-02 15:36:38 -0600

    updated go dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -30,6 +30,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	gatewayv1alpha1 "gateway/api/v1alpha1"
 	gatewayv1alpha2 "gateway/api/v1alpha2"
@@ -72,9 +73,11 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Scheme: scheme,
+		Metrics: metricsserver.Options{
+			BindAddress: metricsAddr,
+		},
+		// Port:                   9443,  //webhook port
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "f2ac972d.netapp.com",
