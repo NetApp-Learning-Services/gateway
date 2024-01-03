561614482f22353834bbef25f07a20e6f76e04b9
cburchett <curtis@burchett.me>
2024-01-03 13:43:47 -0600

    Added docker-registry generation to kustomize build

--- test.sh
+++ test.sh
@@ -8,4 +8,6 @@ kubectl delete namespace gateway-system
 git pull origin v1beta1 #need to update when changing feature branches
 make docker-build docker-push IMG=docker-registry:30001/curtisab/gateway:v1beta1
 make deploy
+# kubectl -n gateway-system create secret docker-registry myreg --docker-server=https://docker-registry:30001 --docker-username=admin --docker-password=Netapp1!
+# kubectl -n gateway-system patch sa gateway-controller-manager -p '{\"imagePullSecrets\": [{\"name\": \"myreg\"}]}'
 kubectl -n gateway-system create -f notes/testCR.yaml
