ba4a79bdcc9d0c97f7586c2c50a37e8fcac52f00
Curtis Burchett <curtis@burchett.me>
2024-12-17 22:18:22 +0000

    First attempt at the S3 API

---
+++ api/v1beta2/s3_types.go
@@ -0,0 +1,62 @@
+package v1beta2
+
+type S3SubSpec struct {
+
+	// Provides required S3 enablement
+	// +kubebuilder:validation:Required
+	Enabled bool `json:"enabled"`
+
+	// Provides required S3 server name
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+
+	// Provides optional S3 LIFs
+	// +kubebuilder:validation:Optional
+	Lifs []LIF `json:"interfaces,omitempty"`
+
+	// Provides optional user definition
+	// +kubebuilder:validation:Optional
+	Users []S3User `json:"users,omitempty"`
+
+	// Provides optional Http definition
+	// +kubebuilder:validation:Optional
+	Http *S3Http `json:"http,omitempty"`
+
+	// Provides optional Https definition
+	// +kubebuilder:validation:Optional
+	Https *S3Https `json:"https,omitempty"`
+}
+
+type S3User struct {
+	// Provides required user name
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+}
+
+type S3Http struct {
+	// Provides required S3 http enablement
+	// +kubebuilder:validation:Required
+	Enabled bool `json:"enabled"`
+
+	// Provides required S3 http enablement
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:=80
+	//Port int `json:"port,omitempty"`
+}
+
+type S3Https struct {
+	// Provides required S3 https enablement
+	// +kubebuilder:validation:Required
+	Enabled bool `json:"enabled"`
+
+	// Provides required S3 https enablement
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:=443
+	//Port int `json:"port,omitempty"`
+}
+
+type S3Tls struct {
+	// Provides required S3 tls enablement
+	// +kubebuilder:validation:Required
+	Enabled bool `json:"enabled"`
+}
