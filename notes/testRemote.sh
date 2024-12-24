267383e3735f7ec1942a7cbc880e6ac5539bad43
Curtis Burchett <curtis@burchett.me>
2024-12-24 22:50:54 +0000

    Update test script to eliminate the sudo for now

--- notes/testRemote.sh
+++ notes/testRemote.sh
@@ -7,7 +7,8 @@ kubectl -n gateway-system delete secret ontap-svmtest-admin
 kubectl delete namespace gateway-system 
 #git pull origin v1beta2 #need to update when changing feature branches
 
-sudo apt install sshpass
+#Uncomment below to install for the first time
+#sudo apt install sshpass
 sshpass -p Netapp1! ssh root@192.168.0.61 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
 sshpass -p Netapp1! ssh root@192.168.0.62 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
 sshpass -p Netapp1! ssh root@192.168.0.63 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
