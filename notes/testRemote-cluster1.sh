50fb337a3a86e99733ca8ac27877f9a7d2a7a09a
Curtis Burchett <curtis@burchett.me>
2024-12-26 18:26:12 +0000

    Trying to fix test script

--- notes/testRemote-cluster1.sh
+++ notes/testRemote-cluster1.sh
@@ -13,8 +13,6 @@ sshpass -p Netapp1! ssh root@192.168.0.61 "ctr -n k8s.io i rm docker.io/curtisab
 sshpass -p Netapp1! ssh root@192.168.0.62 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
 sshpass -p Netapp1! ssh root@192.168.0.63 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
 sshpass -p Netapp1! ssh root@192.168.0.64 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
-sshpass -p Netapp1! ssh root@192.168.0.97 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
-sshpass -p Netapp1! ssh root@192.168.0.98 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
 
 make docker-build docker-push
 make deploy
