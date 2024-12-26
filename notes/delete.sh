94f641973a2d1dc3f105c2d34f8c82808b847b16
Curtis Burchett <curtis@burchett.me>
2024-12-26 18:03:04 +0000

    Cleaning up notes

--- notes/delete.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install sshpass
-sshpass -p Netapp1! ssh root@192.168.0.61 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
-sshpass -p Netapp1! ssh root@192.168.0.62 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
-sshpass -p Netapp1! ssh root@192.168.0.63 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
-sshpass -p Netapp1! ssh root@192.168.0.64 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
-
-
-kubectl delete -f notes/testCR.yaml
--- notes/deleteMirror.sh
+++ notes/deleteMirror.sh
@@ -3,3 +3,6 @@ sshpass -p Netapp1! ssh root@192.168.0.61 "rm /etc/containerd/certs.d/docker.io/
 sshpass -p Netapp1! ssh root@192.168.0.62 "rm /etc/containerd/certs.d/docker.io/hosts.toml"
 sshpass -p Netapp1! ssh root@192.168.0.63 "rm /etc/containerd/certs.d/docker.io/hosts.toml"
 sshpass -p Netapp1! ssh root@192.168.0.64 "rm /etc/containerd/certs.d/docker.io/hosts.toml"
+sshpass -p Netapp1! ssh root@192.168.0.96 "rm /etc/containerd/certs.d/docker.io/hosts.toml"
+sshpass -p Netapp1! ssh root@192.168.0.97 "rm /etc/containerd/certs.d/docker.io/hosts.toml"
+sshpass -p Netapp1! ssh root@192.168.0.98 "rm /etc/containerd/certs.d/docker.io/hosts.toml"
