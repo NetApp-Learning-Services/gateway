588795322da4bba5631aeb067841efd1dd1cd6ff
Curtis Burchett <curtis@burchett.me>
2024-12-26 18:30:06 +0000

    Removing the deleting of container image from master nodes

--- notes/testRemote-cluster1.sh
+++ notes/testRemote-cluster1.sh
@@ -9,7 +9,7 @@ kubectl delete namespace gateway-system
 
 #Uncomment below to install for the first time
 #sudo apt install sshpass
-sshpass -p Netapp1! ssh root@192.168.0.61 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
+#sshpass -p Netapp1! ssh root@192.168.0.61 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
 sshpass -p Netapp1! ssh root@192.168.0.62 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
 sshpass -p Netapp1! ssh root@192.168.0.63 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
 sshpass -p Netapp1! ssh root@192.168.0.64 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
