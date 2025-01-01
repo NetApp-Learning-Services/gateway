499d1d75996bd0ba37ca0e3394bc9f5087d826c5
Curtis Burchett <curtis@burchett.me>
2025-01-01 20:27:31 +0000

    working on test script for cluster2

--- notes/testRemote-cluster2.sh
+++ notes/testRemote-cluster2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # A simple bash script to reset and test the application
 kubectl config use-context destination-admin@destination
-kubectl -n gateway-system delete svm svmtest2
-kubectl -n gateway-system delete secret ontap-cluster2-admin
-kubectl -n gateway-system delete secret ontap-svmtest2-admin
-kubectl delete namespace gateway-system 
+kubectl -n gateway-system delete svm svmdst
+#kubectl -n gateway-system delete secret ontap-cluster2-admin
+#kubectl -n gateway-system delete secret ontap-svmdst-admin
+#kubectl delete namespace gateway-system 
+make undeploy
 #git pull origin v1beta2 #need to update when changing feature branches
 
 #Uncomment below to install for the first time
@@ -13,7 +14,7 @@ kubectl delete namespace gateway-system
 sshpass -p Netapp1! ssh -o StrictHostKeyChecking=no root@192.168.0.97 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
 sshpass -p Netapp1! ssh -o StrictHostKeyChecking=no root@192.168.0.98 "ctr -n k8s.io i rm docker.io/curtisab/gateway:v1beta2"
 
-make docker-build docker-push
+#make docker-build docker-push
 make deploy
 # kubectl -n gateway-system create secret docker-registry myreg --docker-server=https://docker-registry:30001 --docker-username=admin --docker-password=Netapp1!
 # kubectl -n gateway-system patch sa gateway-controller-manager -p '{\"imagePullSecrets\": [{\"name\": \"myreg\"}]}'
